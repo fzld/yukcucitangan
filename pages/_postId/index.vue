12fb6fafb28e063d75df582e2d8f2f5e42eaaaa5
Fachrizaldi Wahyudi <fzlddd@gmail.com>
2020-03-17 17:05:36 +0700

    add blog misc

--- pages/_postId/index.vue
+++ pages/_postId/index.vue
@@ -3,6 +3,8 @@
         <h1 class="title">{{ title }}</h1>
         <p class="subtitle">{{ previewText }}</p>
         <p class="subtitle">{{ content }}</p>
+        <p class="subtitle">{{ paragraph2 }}</p>
+        <p class="subtitle">{{ paragraph3 }}</p>
         
 
         <div class="footer">
@@ -17,14 +19,16 @@
 <script>
 export default {
     asyncData(context) {
-        return context.app.$storyapi.get('cdn/stories/blog/' + context.params.postId, {
+        return context.app.$storyapi.get('cdn/stories/' + context.params.postId, {
             version: 'draft'
         }).then(res => {
             return {
                 blok: res.data.story.content,
                 title: res.data.story.content.title,
                 previewText: res.data.story.content.previewText,
-                content: res.data.story.content.content
+                content: res.data.story.content.content,
+                paragraph2: res.data.story.content.paragraph2,
+                paragraph3: res.data.story.content.paragraph3,
             }
         })
     }
